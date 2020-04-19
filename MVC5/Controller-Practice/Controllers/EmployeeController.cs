354ca139549005a3b4a62893700ea00703a291b2
afshan ramzan <afshan_ramzan@hotmail.com>
2020-04-19 07:43:26 +0500

    1- Controller in mvc

---
+++ MVC5/Controller-Practice/Controllers/EmployeeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Controller_Practice.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public string Profile(int id, string name)
+        {
+            return "ID = " + id + " and Name = " + name + " .";
+        }
+
+
+        public string FullName(string first = null, string last = null)
+        {
+            if (first != null && last != null)
+            {
+                return "your full name is " + first + " " + last;
+            }
+            else if (first == null)
+            {
+                return "your full name is " + last;
+            }
+            else
+            {
+                return "your full name is " + first;
+            }
+        }
+
+    }
+}
---
+++ MVC5/Controller-Practice/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Controller_Practice.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public string index()
+        {
+            return "i am from home controller index method";
+        }
+
+       
+    }
+}
