9215039ade664493a2fe7b74f0a2c350dc613ad0
afshan ramzan <afshan_ramzan@hotmail.com>
2020-04-19 07:48:50 +0500

    3- Model in MVC

---
+++ MVC5/Model-Practice/Controllers/EmployeeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Model_Practice.Models;
+
+namespace Model_Practice.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public ActionResult Profile()
+        {
+            Employee em = getdata();
+            return View(em);
+
+        }
+
+        private Employee getdata()
+        {
+            return new Employee()
+            {
+                ID = 1,
+                Name = "afshan"
+            };
+        }
+    }
+}
