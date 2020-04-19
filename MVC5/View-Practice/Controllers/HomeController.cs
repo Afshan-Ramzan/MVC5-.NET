9d30b4340b22ebebb7935106cbec007536632879
afshan ramzan <afshan_ramzan@hotmail.com>
2020-04-19 07:45:53 +0500

    2-View in MVC

---
+++ MVC5/View-Practice/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace View_Practice.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult Aboutus()
+        {
+            return View();
+        }
+
+        public ActionResult Contactus()
+        {
+            return View();
+        }
+
+    }
+}
